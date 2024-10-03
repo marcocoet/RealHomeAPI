b661466f19320b1e085b02aa372ffa8f590e73f6
marcocoet <marcoet2019@gmail.com>
2024-10-03 14:41:42 +0200

    fixed migration issues

--- apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RealhomebackendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'realhomebackend'
--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from realhomebackend.views import RealEstateTypeListAPIView
-
-urlpatterns = [
-    path('realestatetype/list/', RealEstateTypeListAPIView.as_view())
-]
