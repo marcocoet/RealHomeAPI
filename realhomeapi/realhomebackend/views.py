3974c01215eaa71b961ac8031dbd8947939d4b0e
marcocoet <marcoet2019@gmail.com>
2024-10-11 09:00:44 +0200

    realestateaddProperty modesls views ens

--- realhomeapi/realhomebackend/views.py
+++ realhomeapi/realhomebackend/views.py
@@ -1,18 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Realestate_All_Buy
+
 from rest_framework import serializers
 from .models import RealEstateType
-from .models import Realestate_All_Buy
+from .models import RealEstate
+
 
 # Create your views here.
 
-class RealEstateTypeListAPIView(APIView):
-    def get(self, request):
-        properties = Realestate_All_Buy.objects.all()
-        serializer = PropertiesSerializer(properties, many=True)
-        return Response(serializer.data)
+
 
 
 class RealEstateTypeSerializer (serializers.ModelSerializer):
@@ -20,14 +17,30 @@ class RealEstateTypeSerializer (serializers.ModelSerializer):
         model = RealEstateType
         fields =  '__all__'
 
-class Realestate_All_BuyAPIView(APIView):
     def get(self, request):
-        types = Realestate_All_Buy.objects.all()
-        serializer = PropertiesSerializer(types, many=True)
-        return Response(serializer.data)
+class RealEstateAddAPIView(APIView):
+    def post(self, request):
+        print("Hallo1")
+        serializer = RealEstateAddSerializer(data=request.data)
+        print("Hallo1a")
+        if serializer.is_valid():
+            serializer.save()
+            print("Hallo2")
+            #savedRealEstate = serializer.instance
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
 
-class PropertiesSerializer (serializers.ModelSerializer):
+class RealEstateAddSerializer (serializers.ModelSerializer):
     class Meta:
-        model = Realestate_All_Buy
+        model = RealEstate
+        fields =  ['address', 'bathrooms', 'bedrooms', 'landSqm',
+                   'floorSqm', 'parking', 'shortDescription', 'longDescription']
+
+class RealEstateTypeSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = RealEstateType
         fields =  '__all__'
+
+
     
